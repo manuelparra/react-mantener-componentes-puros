0b242d300d91ae2ea9d186a8105a05e3a3bf692e
Manuel Parra <manuel@macbook-air.local>
2022-12-22 10:29:20 +0000

    Secund commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+const Recipe = ({ drinkers }) => {
+  return (
+    <ol>
+      <li>Boil {drinkers} cups of milk.</li>
+      <li>Add {2 * drinkers} spoons of masala spices.</li>
+      <li>Remove from heat, and add {drinkers} spoons of tea.</li>
+    </ol>
+  )
+}
+
+const Cup = ({ guest }) => {
+  return <h5>Tea cup for gues #{guest}</h5>;
+}
+
+const TeaGathering = () => {
+  let cups = [];
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />)
+  }
+
+  return cups;
+}
 
 const App = () => {
   return (
-    <div>
-      Hello world!
-    </div> 
+    <section>
+      <h1>Spiced chai recipe.</h1>
+      <h2>For one</h2>
+      <Recipe drinkers={1} />
+      <h2>For a gathering.</h2>
+      <Recipe drinkers={4} />
+      <h2>Tea cup for guest.</h2>
+      <TeaGathering />
+    </section> 
   );
 }
 
